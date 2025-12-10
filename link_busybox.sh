cd69c5f3f486a940dd14e16dbc1eb1609b3f3b8a
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-12-10 12:44:23 -0600

    ops

--- link_busybox.sh
+++ link_busybox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Creating busybox symlinks"
 cd initramfs/bin
-for t in $(./busybox --list); ln -s busybox $t; done
+for t in $(./busybox --list); do ln -s busybox $t; done
 cd ..
 ln -s bin/busybox init
 cd -
